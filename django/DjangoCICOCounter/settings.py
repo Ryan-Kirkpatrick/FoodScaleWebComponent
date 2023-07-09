2806f0274427aec34c098fe1922f8e7e7c92c8c2
Ryan Kirkpatrick <ryan.kptk@outlook.com>
2023-07-09 20:28:43 +1000

    Reorganized files

--- django/DjangoCICOCounter/settings.py
+++ django/DjangoCICOCounter/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoScale.urls'
+ROOT_URLCONF = 'DjangoCICOCounter.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoScale.wsgi.application'
+WSGI_APPLICATION = 'DjangoCICOCounter.wsgi.application'
 
 
 # Database
--- django/DjangoCICOCounter/wsgi.py
+++ django/DjangoCICOCounter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoScale.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoCICOCounter.settings')
 
 application = get_wsgi_application()
